2032642faba3d91de8dd0eb4bf9441fe7a716e86
Uuganbayar <aurotones@gmail.com>
2023-12-29 16:37:57 +0800

    Project initialize

---
+++ src/main/index.ts
@@ -0,0 +1,51 @@
+import { join } from "path";
+import { app, shell, screen, BrowserWindow } from "electron";
+import { electronApp, optimizer, is } from "@electron-toolkit/utils";
+import icon from "../../resources/icon.png?asset";
+
+async function createWindow(): Promise<void> {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+    const mainWindow = new BrowserWindow({
+        minWidth: 900,
+        minHeight: 670,
+        width: width,
+        height: height,
+        show: false,
+        autoHideMenuBar: true,
+        ...(process.platform === "linux" ? {icon} : {}),
+        webPreferences: {
+            preload: join(__dirname, "../preload/index.js"),
+            sandbox: false
+        }
+    });
+
+    mainWindow.on("ready-to-show", () => {
+        mainWindow.show();
+    });
+
+    mainWindow.webContents.setWindowOpenHandler((details) => {
+        shell.openExternal(details.url)
+        return {
+            action: "deny",
+        }
+    });
+
+    if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+        await mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"])
+    } else {
+        await mainWindow.loadFile(join(__dirname, "../renderer/index.html"))
+    }
+}
+
+app.whenReady().then(async () => {
+    electronApp.setAppUserModelId("com.electron")
+    app.on("browser-window-created",(_, window) => {
+        optimizer.watchWindowShortcuts(window)
+    })
+    await createWindow();
+});
+
+app.on("window-all-closed",() => {
+    app.quit()
+});
